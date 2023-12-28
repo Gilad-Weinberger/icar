c6ae9b2d63cf6f30170a89ab1c76f576f14594fe
Gilad Weinberger <giladwe21@gmail.com>
2023-12-28 22:18:44 +0200

    allowed hosts

--- icar/settings.py
+++ icar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-hp3)zz3zg6#4_ixlejp8o2m%d&&dvoc%@@&d79v-qtw*j86kw5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
